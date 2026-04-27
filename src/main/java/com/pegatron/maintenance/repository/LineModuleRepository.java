fa21f11b93cf916b1782dc3dcfdbc92f5e2ba842
teran210c <162738757+teran210c@users.noreply.github.com>
2026-04-27 10:40:46 -0600

    Overload findByLineId method to include MaintenanceType

--- src/main/java/com/pegatron/maintenance/repository/LineModuleRepository.java
+++ src/main/java/com/pegatron/maintenance/repository/LineModuleRepository.java
@@ -1,6 +1,7 @@
 package com.pegatron.maintenance.repository;
 
 import com.pegatron.maintenance.model.LineModule;
+import com.pegatron.maintenance.model.MaintenanceType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,5 +9,5 @@ import java.util.List;
 public interface LineModuleRepository
         extends JpaRepository<LineModule, Long> {
 
-    List<LineModule> findByLineId(Long lineId);
+    List<LineModule> findByLineId(Long lineId, MaintenanceType type);
 }
