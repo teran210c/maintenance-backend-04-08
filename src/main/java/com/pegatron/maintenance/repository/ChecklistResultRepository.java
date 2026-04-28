4b88353dabee6cc15c2bd39b2543c93d106b1854
teran210c <162738757+teran210c@users.noreply.github.com>
2026-04-28 13:29:39 -0600

    Refactor ChecklistResultRepository to ChecklistTemplateRepository

--- src/main/java/com/pegatron/maintenance/repository/ChecklistResultRepository.java
+++ src/main/java/com/pegatron/maintenance/repository/ChecklistResultRepository.java
@@ -1,16 +1,15 @@
 package com.pegatron.maintenance.repository;
 
-import com.pegatron.maintenance.model.ChecklistResult;
+import com.pegatron.maintenance.model.ChecklistTemplate;
+import com.pegatron.maintenance.model.MaintenanceType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface ChecklistResultRepository
-        extends JpaRepository<ChecklistResult, Long> {
+public interface ChecklistTemplateRepository
+        extends JpaRepository<ChecklistTemplate, Long> {
 
-    List<ChecklistResult> findByModule_Id(Long moduleId);
-    Optional<ChecklistResult> findByModule_IdAndItemName(Long moduleId, String itemName);
-    void deleteByModule_Maintenance_Id(Long maintenanceId);
+    List<ChecklistTemplate> findByModuleName(String moduleName);
+    List<ChecklistTemplate> findByModuleNameAndMaintenanceType(String moduleName, MaintenanceType maintenanceType);
 
 }
