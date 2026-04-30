1a30b69360777e0326e826dd6d7206bcdc90f154
teran210c <162738757+teran210c@users.noreply.github.com>
2026-04-30 14:47:50 -0600

    Update ChecklistResultController.java

--- src/main/java/com/pegatron/maintenance/controller/ChecklistResultController.java
+++ src/main/java/com/pegatron/maintenance/controller/ChecklistResultController.java
@@ -1,6 +1,11 @@
 package com.pegatron.maintenance.controller;
 
 import com.pegatron.maintenance.model.ChecklistResult;
+import com.pegatron.maintenance.model.ChecklistTemplate;
+import com.pegatron.maintenance.model.MaintenanceTask;
+import com.pegatron.maintenance.model.MaintenanceType;
+import com.pegatron.maintenance.repository.ChecklistResultRepository;
+import com.pegatron.maintenance.repository.ChecklistTemplateRepository;
 import com.pegatron.maintenance.service.ChecklistResultService;
 import jakarta.transaction.Transactional;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -15,13 +20,18 @@ public class ChecklistResultController {
 
     private final ChecklistResultService service;
     private final SimpMessagingTemplate messagingTemplate;
+    private final ChecklistResultRepository checklistResultRepository;
+    private  final ChecklistTemplateRepository checklistTemplateRepository;
 
     public ChecklistResultController(
             ChecklistResultService service,
-            SimpMessagingTemplate messagingTemplate
+            SimpMessagingTemplate messagingTemplate,
+            ChecklistResultRepository checklistResultRepository, ChecklistTemplateRepository checklistTemplateRepository
     ) {
         this.service = service;
         this.messagingTemplate = messagingTemplate;
+        this.checklistResultRepository = checklistResultRepository;
+        this.checklistTemplateRepository = checklistTemplateRepository;
     }
 
     @GetMapping("/{moduleId}")
@@ -50,6 +60,19 @@ public class ChecklistResultController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         service.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public ChecklistResult updateTask(
+            @PathVariable Long id,
+            @RequestBody Map<String, String> body
+    ) {
+     String newName = body.get("itemName");
+     String oldName = body.get("oldItemName");
+     String moduleName = body.get("moduleName");
+     MaintenanceType type = MaintenanceType.valueOf(body.get("maintenanceType"));
+
+     return service.updateTask(id, newName, oldName, moduleName, type);
     }
 
 }
